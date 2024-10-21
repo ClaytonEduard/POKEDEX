a3b64633ae6ee47ab6763a914b7f243ac7069375
ClaytonEduard <clayton_eduard@hotmail.com>
2024-10-21 13:02:32 -0300

    Criando o um modelo personalizado de detalhes 2 finalizado

--- assets/js/poke.api.js
+++ assets/js/poke.api.js
@@ -1,8 +1,28 @@
 const pokeapi = {};
 
+// converter o modelo da pokeApi para um modelo personalizado
+function convertPokeApiDetailsToPokemon(pokeDetail) {
+  const pokemon = new Pokemon();
+  pokemon.number = pokeDetail.order;
+  pokemon.name = pokeDetail.name;
+
+  //destruct
+  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+  const [type] = types;
+
+  pokemon.types = types;
+  pokemon.type = type; // pega o primeiro elemento do array
+
+  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+  return pokemon;
+}
+
 // convert lista de promessas "Promise.ALL()" para pegar cada detalhe
 pokeapi.getPokemonsDetails = (pokemon) => {
-  return fetch(pokemon.url).then((response) => response.json());
+  return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailsToPokemon); // retornar o Poke personalizada
 };
 
 pokeapi.getPokemons = (offset = 0, limit = 5) => {
