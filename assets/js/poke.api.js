8e7f42464da7378732c6075bcd027ead25cea792
ClaytonEduard <clayton_eduard@hotmail.com>
2024-10-21 15:24:47 -0300

    melhorando o css e outros

--- assets/js/poke.api.js
+++ assets/js/poke.api.js
@@ -25,11 +25,11 @@ pokeapi.getPokemonsDetails = (pokemon) => {
     .then(convertPokeApiDetailsToPokemon); // retornar o Poke personalizada
 };
 
-pokeapi.getPokemons = (offset = 0, limit = 5) => {
+pokeapi.getPokemons = (offset = 0, limit = 20) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
   return fetch(url)
     .then((response) => response.json()) // convert JSON response
-    .then((jsonBody) => jsonBody.results) // capiture do body
+    .then((jsonBody) => jsonBody.results) // capture da lista no body
     .then((pokemons) => pokemons.map(pokeapi.getPokemonsDetails)) // convert lista de promessas "Promise.ALL()" para pegar cada detalhe
     .then((detailRequests) => Promise.all(detailRequests)) // Esperar a lista ser resolvida
     .then((pokemonDetails) => pokemonDetails) //retorna a lista de pokemon
